e063e0c8934df318d70108a75bd0256e8efd5121
쌈바 (ChangWooKim) <ahimahas@coupang.com>
2016-12-16 16:55:42 +0900

    upload BubbleSort

---
+++ Sorts/BubbleSort/BubbleSort.swift
@@ -0,0 +1,33 @@
+//
+//  BubbleSort.swift
+//  HeadQuarter
+//
+//  Created by ahimahas on 2016. 12. 16..
+//  Copyright © 2016년 MS. All rights reserved.
+//
+
+import Foundation
+
+class BubbleSort {
+    // MARK: - Internal Methods
+    func sort(withItems items: [Int]) -> [Int] {
+        var copiedItems: [Int] = items
+        
+        for i in (0..<copiedItems.count).reversed() {
+            for j in 0..<i {
+                if copiedItems[j] > copiedItems[j + 1] {
+                    swap(left: &copiedItems[j], right:&copiedItems[j + 1])
+                }
+            }
+        }
+        
+        return copiedItems
+    }
+    
+    // MARK: - Private Methods
+    private func swap(left: inout Int, right: inout Int) {
+        let temp = left
+        left = right
+        right = temp
+    }
+}
